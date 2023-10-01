d37bbf05166222a81c0c819c0e046846e451f5de
harsweet <64262868+harsweet@users.noreply.github.com>
2023-10-01 02:11:07 -0600

    Extracts methods out of the long main method

--- StartGame.java
+++ StartGame.java
@@ -1,15 +1,36 @@
 import java.util.Scanner;
 
 public class StartGame {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    static  Scanner scanner = new Scanner(System.in);
 
+    private Player getGamePlayer() {
         System.out.println("Welcome to the Guess the Number game!");
-
         System.out.print("Enter your name: ");
+        
         String playerName = scanner.nextLine();
         Player player = new Player(playerName);
 
+        return player;
+    }
+
+    private int getGuess() {
+        System.out.print("Enter your guess: ");
+        int guess = scanner.nextInt();
+        scanner.nextLine();  // Consume the newline character
+
+        return guess;
+    }
+
+    private String getNewGameResponse(Player currPlayer, int attempts) {
+        System.out.println(currPlayer.getName() + ", you guessed the number in " + attempts + " attempts.");
+        System.out.println("Your score: " + currPlayer.getScore());
+
+        System.out.print("Play again? (yes/no): ");
+        String playAgainInput = scanner.nextLine().toLowerCase();
+        return playAgainInput;
+    }
+
+    private void simulateGame(Player player) {
         boolean playAgain = true;
         while (playAgain) {
             Game game = new Game();
@@ -18,24 +39,25 @@ public class StartGame {
 
             System.out.println("I'm thinking of a number between 1 and 100.");
             while (!gameOver) {
-                System.out.print("Enter your guess: ");
-                int guess = scanner.nextInt();
-                scanner.nextLine();  // Consume the newline character
-
-                gameOver = game.guess(guess);
+                gameOver = game.guess(getGuess());
                 attempts++;
             }
 
             player.increaseScore();
-            System.out.println(player.getName() + ", you guessed the number in " + attempts + " attempts.");
-            System.out.println("Your score: " + player.getScore());
-
-            System.out.print("Play again? (yes/no): ");
-            String playAgainInput = scanner.nextLine().toLowerCase();
-            playAgain = playAgainInput.equals("yes");
+            playAgain = getNewGameResponse(player, attempts).equals("yes");
         }
-
         System.out.println("Thanks for playing!");
+    }
+
+    private void playGame() {
+        Player gamePlayer = getGamePlayer();
+
+        simulateGame(gamePlayer);
         scanner.close();
+    }
+
+    public static void main(String[] args) {
+        StartGame newGame = new StartGame();
+        newGame.playGame();
     }
 }
